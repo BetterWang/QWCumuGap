61beb88799fd3c18398061b64b849ad5a849871d
Quan Wang <BetterWang@gmail.com>
2018-03-16 20:27:01 +0100

    [+] more config

--- test/crab_cumu_MB_sysPhiAcc_reverse_v1.py
+++ test/crab_cumu_MB_sysPhiAcc_reverse_v1.py
@@ -5,14 +5,14 @@ from httplib import HTTPException
 
 config = config()
 
-config.General.requestName = 'PAMB1_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB1_cumugap_eff_sysPhiAcc_reverse_v2'
 config.General.workArea = 'CrabArea'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'qwcumu_pPb16_MB_eff_sysPhiAcc_v1.py'
 config.Data.inputDataset = '/PAMinimumBias1/PARun2016C-PromptReco-v1/AOD'
-config.JobType.inputFiles = ['Hijing_8TeV_MB_Narrow2.root', 'phiMB2_merge.root']
+config.JobType.inputFiles = ['Hijing_8TeV_dataBS.root', 'phiMB2_merge.root']
 #config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
@@ -33,7 +33,7 @@ except ClientException as cle:
 
 
 ##################2
-config.General.requestName = 'PAMB2_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB2_cumugap_eff_sysPhiAcc_reverse_v2'
 config.Data.inputDataset = '/PAMinimumBias2/PARun2016C-PromptReco-v1/AOD'
 try:
         crabCommand('submit', config = config)
@@ -45,7 +45,7 @@ except ClientException as cle:
 
 
 ##################3
-config.General.requestName = 'PAMB3_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB3_cumugap_eff_sysPhiAcc_reverse_v2'
 config.Data.inputDataset = '/PAMinimumBias3/PARun2016C-PromptReco-v1/AOD'
 try:
         crabCommand('submit', config = config)
@@ -58,7 +58,7 @@ except ClientException as cle:
 
 
 ##################4
-config.General.requestName = 'PAMB4_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB4_cumugap_eff_sysPhiAcc_reverse_v2'
 config.Data.inputDataset = '/PAMinimumBias4/PARun2016C-PromptReco-v1/AOD'
 try:
         crabCommand('submit', config = config)
@@ -70,7 +70,7 @@ except ClientException as cle:
 
 
 ##################5
-config.General.requestName = 'PAMB5_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB5_cumugap_eff_sysPhiAcc_reverse_v2'
 config.Data.inputDataset = '/PAMinimumBias5/PARun2016C-PromptReco-v1/AOD'
 try:
         crabCommand('submit', config = config)
@@ -85,7 +85,7 @@ except ClientException as cle:
 
 
 ##################6
-config.General.requestName = 'PAMB6_cumugap_eff_sysPhiAcc_reverse_v1'
+config.General.requestName = 'PAMB6_cumugap_eff_sysPhiAcc_reverse_v2'
 config.Data.inputDataset = '/PAMinimumBias6/PARun2016C-PromptReco-v1/AOD'
 try:
         crabCommand('submit', config = config)
